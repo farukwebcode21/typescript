0f6335ceae0167e55450b487a984b2cdec5c481a
farukwebcode21 <farukahmadmk@yahoo.com>
2021-09-27 14:17:38 +0600

    Learning typescript

---
+++ output/first.js
@@ -0,0 +1,37 @@
+"use strict";
+// let playerName;
+// playerName = 'Mashrafi';
+// let age = 26;
+// console.log(playerName);
+// playerName = 34;
+// console.log(playerName);
+// console.log(age);
+/* function multiply(a:number, b:number){
+    return a * b;
+}
+console.log(multiply(4, 6));
+
+
+let fruits = ['apple', 'Banana', 'orange', 'Pineapple'];
+ */
+/* let b:string | number;
+b = 'Faruk';
+ */
+const countryName = "Bangladesh";
+console.log(countryName);
+function add(x, y) {
+    return x + y;
+}
+let result = add(20, 10);
+console.log(result);
+let message = 'Hello World Love You Typescript!';
+let heading = document.createElement('h1');
+heading.textContent = message;
+document.body.appendChild(heading);
+// Another Example 
+let box;
+console.log(typeof (box));
+box = "Hello"; //Checking string 
+console.log(typeof (box));
+box = 1000;
+console.log(typeof (box));
